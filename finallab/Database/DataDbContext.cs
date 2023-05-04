ee0ce6bbdcfa56ef499cac54b5c72157491bffe2
ASUS <ASUS@LAPTOP-01J23IO5>
2023-05-04 09:42:27 +0700

    Add project files.

---
+++ finallab/Database/DataDbContext.cs
@@ -0,0 +1,16 @@
+﻿using finallab.Models;
+using Microsoft.EntityFrameworkCore; //import Ef
+using System.Collections.Generic;
+
+namespace finallab.Database
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
+
+        public DbSet<employees> employees { get; set; }
+
+        public DbSet<positions> positions { get; set; }
+    }
+}
+
